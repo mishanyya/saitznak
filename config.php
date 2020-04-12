efdb1bec8c3d2bd32c53c5ec2c09fd04bb9af345
mishanyya <mishanyakashin@gmail.com>
2020-04-12 13:54:17 +0400

    изменил файл .gitignore 120420 1354

--- config.php
+++ config.php
@@ -4,10 +4,7 @@ $user_name = "saitznak_admin";
 $user_password = "10132031";
 $db_name="saitznak";
 
- $folder = '/home/mishanyya/public_html/moy_site/modredpol/fotosait/';//папка для загрузки файлов lubuntu
-//  $folder = '/home/u284627933/public_html/modredpol/fotosait/';//папка для загрузки файлов vmesteprosto.info
-//$folder = '/server/www/modredpol/fotosait/';//папка для загрузки файлов дома на localhost
-//$folder = '/home/localhost/www/modredpol/fotosait/';//папка для загрузки файлов на ноутбуке
+ 
 
 $folder1 = '/modredpol/fotosait/';//папка для выгрузки файлов
 $netfoto="/modredpol/fotosait/fotonet.png";//рисунок замещает отсутствие фото
