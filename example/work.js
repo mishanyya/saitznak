4cab9528e854e6c5fb649a50f9b4d24936c231ba
mishanyya <mishanyakashin@gmail.com>
2021-06-13 19:39:44 +0300

    работа с регулярными выражениями на PHP наброски 19.39 13 июня 2021

---
+++ example/work.js
@@ -0,0 +1,27 @@
+function functionforajax(){
+//создать объект для запроса к серверу
+var req = getXmlHttp();
+
+//указать блок в который будут вводиться исходные данные
+var inputData= document.getElementById('RegExpInput').value;
+//указать блок в который будет выводиться результат
+var outputData= document.getElementById('RegExpOutput');
+
+//код, работающий с PHP
+req.onreadystatechange = function() {
+if (req.readyState == 4) {
+if(req.status == 200) {
+// если статус 200 (ОК) - выдать ответ пользователю
+//вывести результат в указанный блок
+outputData.innerHTML=req.responseText;
+
+}}}
+
+//в одном файле рекомендуется одновременно использовать только один из запросов GET и POST
+
+//запрос к файлу PHP с передачей значений переменных методом GET
+//inputData - значение переменной, которое будет передано на сервер
+
+req.open('GET', 'work1.php?inputData='+inputData, true);
+req.send(null); // отослать запрос
+}
